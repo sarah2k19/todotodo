e50863627c8e86cad213321612d7b32f8ca48c9e
webtech2k16 <s.n.khalil@student.tue.nl>
2016-12-06 16:55:30 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,107 +1,106 @@
-	// Delete and check/complete icons in SVG format
-	var deleteIcon = '<svg version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px" viewBox="0 0 22 22" style="enable-background:new 0 0 22 22;" xml:space="preserve"><rect class="noFill" width="22" height="22"/><g><g><path class="fill" d="M16.1,3.6h-1.9V3.3c0-1.3-1-2.3-2.3-2.3h-1.7C8.9,1,7.8,2,7.8,3.3v0.2H5.9c-1.3,0-2.3,1-2.3,2.3v1.3c0,0.5,0.4,0.9,0.9,1v10.5c0,1.3,1,2.3,2.3,2.3h8.5c1.3,0,2.3-1,2.3-2.3V8.2c0.5-0.1,0.9-0.5,0.9-1V5.9C18.4,4.6,17.4,3.6,16.1,3.6z M9.1,3.3c0-0.6,0.5-1.1,1.1-1.1h1.7c0.6,0,1.1,0.5,1.1,1.1v0.2H9.1V3.3z M16.3,18.7c0,0.6-0.5,1.1-1.1,1.1H6.7c-0.6,0-1.1-0.5-1.1-1.1V8.2h10.6V18.7z M17.2,7H4.8V5.9c0-0.6,0.5-1.1,1.1-1.1h10.2c0.6,0,1.1,0.5,1.1,1.1V7z"/></g><g><g><path class="fill" d="M11,18c-0.4,0-0.6-0.3-0.6-0.6v-6.8c0-0.4,0.3-0.6,0.6-0.6s0.6,0.3,0.6,0.6v6.8C11.6,17.7,11.4,18,11,18z"/></g><g><path class="fill" d="M8,18c-0.4,0-0.6-0.3-0.6-0.6v-6.8c0-0.4,0.3-0.6,0.6-0.6c0.4,0,0.6,0.3,0.6,0.6v6.8C8.7,17.7,8.4,18,8,18z"/></g><g><path class="fill" d="M14,18c-0.4,0-0.6-0.3-0.6-0.6v-6.8c0-0.4,0.3-0.6,0.6-0.6c0.4,0,0.6,0.3,0.6,0.6v6.8C14.6,17.7,14.3,18,14,18z"/></g></g></g></svg>';
-	var checkIcon = '<svg version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px" viewBox="0 0 22 22" style="enable-background:new 0 0 22 22;" xml:space="preserve"><rect y="0" class="noFill" width="22" height="22"/><g><path class="fill" d="M9.7,14.4L9.7,14.4c-0.2,0-0.4-0.1-0.5-0.2l-2.7-2.7c-0.3-0.3-0.3-0.8,0-1.1s0.8-0.3,1.1,0l2.1,2.1l4.8-4.8c0.3-0.3,0.8-0.3,1.1,0s0.3,0.8,0,1.1l-5.3,5.3C10.1,14.3,9.9,14.4,9.7,14.4z"/></g></svg>';
+// Delete and check/complete icons in SVG format
+var deleteIcon = '<svg version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px" viewBox="0 0 22 22" style="enable-background:new 0 0 22 22;" xml:space="preserve"><rect class="noFill" width="22" height="22"/><g><g><path class="fill" d="M16.1,3.6h-1.9V3.3c0-1.3-1-2.3-2.3-2.3h-1.7C8.9,1,7.8,2,7.8,3.3v0.2H5.9c-1.3,0-2.3,1-2.3,2.3v1.3c0,0.5,0.4,0.9,0.9,1v10.5c0,1.3,1,2.3,2.3,2.3h8.5c1.3,0,2.3-1,2.3-2.3V8.2c0.5-0.1,0.9-0.5,0.9-1V5.9C18.4,4.6,17.4,3.6,16.1,3.6z M9.1,3.3c0-0.6,0.5-1.1,1.1-1.1h1.7c0.6,0,1.1,0.5,1.1,1.1v0.2H9.1V3.3z M16.3,18.7c0,0.6-0.5,1.1-1.1,1.1H6.7c-0.6,0-1.1-0.5-1.1-1.1V8.2h10.6V18.7z M17.2,7H4.8V5.9c0-0.6,0.5-1.1,1.1-1.1h10.2c0.6,0,1.1,0.5,1.1,1.1V7z"/></g><g><g><path class="fill" d="M11,18c-0.4,0-0.6-0.3-0.6-0.6v-6.8c0-0.4,0.3-0.6,0.6-0.6s0.6,0.3,0.6,0.6v6.8C11.6,17.7,11.4,18,11,18z"/></g><g><path class="fill" d="M8,18c-0.4,0-0.6-0.3-0.6-0.6v-6.8c0-0.4,0.3-0.6,0.6-0.6c0.4,0,0.6,0.3,0.6,0.6v6.8C8.7,17.7,8.4,18,8,18z"/></g><g><path class="fill" d="M14,18c-0.4,0-0.6-0.3-0.6-0.6v-6.8c0-0.4,0.3-0.6,0.6-0.6c0.4,0,0.6,0.3,0.6,0.6v6.8C14.6,17.7,14.3,18,14,18z"/></g></g></g></svg>';
 
-	// User clicked on the add button
-	// If there is any text inside the checkListItem field, add that text to the todo list
-	document.getElementById('add').addEventListener('click', function() {
-		var value = document.getElementById('checkListItem').value;
-		if (value) {
-			addItemTodo(value);
-			document.getElementById('checkListItem').value = '';
-		}
-	});
+// User clicked on the add button
+// If there is any text inside the checkListItem field, add that text to the todo list
+document.getElementById('add').addEventListener('click', function() {
+    var value = document.getElementById('checkListItem').value;
+    if (value) {
+        addItemTodo(value);
+        document.getElementById('checkListItem').value = '';
+    }
+});
 
-	function removeItem() {
-		var item = this.parentNode.parentNode;
-		var parent = item.parentNode;
+function removeItem() {
+    var item = this.parentNode.parentNode;
+    var parent = item.parentNode;
 
-		parent.removeChild(item);
+    parent.removeChild(item);
 
-		// refresh item list for storage
-		addItemsToArray();
-	}
+    // refresh item list for storage
+    addItemsToArray();
+}
 
 //document.body.onload = addItemTodo;
-	// Adds a new item to the todo list
-	function addItemTodo(text) {
+// Adds a new item to the todo list
+function addItemTodo(text) {
 
-		var list = document.getElementById('todo');
+    var list = document.getElementById('todo');
 
-		var item = document.createElement('li');
-		item.innerText = text;
+    var item = document.createElement('li');
+    item.innerText = text;
 
-		var buttons = document.createElement('div');
-		buttons.classList.add('buttons');
+    var buttons = document.createElement('div');
+    buttons.classList.add('buttons');
 
-		var remove = document.createElement('button');
-		remove.classList.add('remove');
-		remove.innerHTML = deleteIcon;
+    var remove = document.createElement('button');
+    remove.classList.add('remove');
+    remove.innerHTML = deleteIcon;
 
-		// Add click event for removing the item
-		remove.addEventListener('click', removeItem);
+    // Add click event for removing the item
+    remove.addEventListener('click', removeItem);
 
-		// Add click event for completing the item
-		buttons.appendChild(remove);
-		item.appendChild(buttons);
+    // Add click event for completing the item
+    buttons.appendChild(remove);
+    item.appendChild(buttons);
 
-		list.insertBefore(item, list.childNodes[0]);
+    list.insertBefore(item, list.childNodes[0]);
 
-		// refresh item list for storage
-    	addItemsToArray();
-	}
+    // refresh item list for storage
+    addItemsToArray();
+}
 
 
-	function addItemsToArray(){
-		// create empty array
-		var itemArray = [];
+function addItemsToArray() {
+    // create empty array
+    var itemArray = [];
 
-		// add all list items to array
-		$("#todo li").each(function() {
-			var listItem = $(this).text();
-			itemArray.push(listItem);
-		});
+    // add all list items to array
+    $("#todo li").each(function() {
+        var listItem = $(this).text();
+        itemArray.push(listItem);
+    });
 
-		// parse all array items to string and into array
-		localStorage.setItem("list", JSON.stringify(itemArray));
-	}
-  
-  function retrieveStoredItems(){
-  	// get parsed array back from storage
-  	var storedList = JSON.parse(localStorage.getItem("list"));
-  	
-  	// foreach array item create new element
-  	$.each(storedList, function(i, val) {
-  		var list = document.getElementById('todo');
+    // parse all array items to string and into array
+    localStorage.setItem("list", JSON.stringify(itemArray));
+}
 
-		var item = document.createElement('li');
-		item.innerText = val;
+function retrieveStoredItems() {
+    // get parsed array back from storage
+    var storedList = JSON.parse(localStorage.getItem("list"));
 
-		var buttons = document.createElement('div');
-		buttons.classList.add('buttons');
+    // foreach array item create new element
+    $.each(storedList, function(i, val) {
+        var list = document.getElementById('todo');
 
-		var remove = document.createElement('button');
-		remove.classList.add('remove');
-		remove.innerHTML = deleteIcon;
+        var item = document.createElement('li');
+        item.innerText = val;
 
-		// Add click event for removing the item
-		remove.addEventListener('click', removeItem);
+        var buttons = document.createElement('div');
+        buttons.classList.add('buttons');
 
-		// Add click event for completing the item
-		buttons.appendChild(remove);
-		item.appendChild(buttons);
+        var remove = document.createElement('button');
+        remove.classList.add('remove');
+        remove.innerHTML = deleteIcon;
 
-		list.insertBefore(item, list.childNodes[0]);
-	});
+        // Add click event for removing the item
+        remove.addEventListener('click', removeItem);
 
-  }
+        // Add click event for completing the item
+        buttons.appendChild(remove);
+        item.appendChild(buttons);
 
-  // on load of document
-	$( function() {
-		// add sortable to list items
-	    $( "#todo" ).sortable();
-	    // disable selection for list items
-	    $( "#todo" ).disableSelection();
-	    // retrieve stored items
-	    retrieveStoredItems();
-  	} );
+        list.insertBefore(item, list.childNodes[0]);
+    });
+
+}
+
+// on load of document
+$(function() {
+    // add sortable to list items
+    $("#todo").sortable();
+    // disable selection for list items
+    $("#todo").disableSelection();
+    // retrieve stored items
+    retrieveStoredItems();
+});
